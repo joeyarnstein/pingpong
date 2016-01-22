d8dd87c81142f675e4379c50bfa66978cd8c30ef
Joey <joeyarnstein@gmail.com>
2016-01-22 10:28:37 -0800

    add functionality to all multiples of 3, 5 and 15 in scripts.js and specs.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 var pingpong = function(countTo){
   var returnArray = [];
   for(var index = 1; index <= countTo; index += 1)
-  if (index % 3 === 0) {
-    returnArray.push("ping");
-  } else {
-  returnArray.push(index);
-  }
+    if (index % 15 === 0){
+      returnArray.push("pingpong");
+    } else if (index % 3 === 0) {
+      returnArray.push("ping");
+    } else if (index % 5 === 0){
+      returnArray.push("pong");
+    } else {
+      returnArray.push(index);
+    }
   return returnArray;
 }
--- specs/specs.js
+++ specs/specs.js
@@ -7,7 +7,23 @@ describe("pingpong", function() {
     expect(pingpong(4)).to.eql([1, 2, 'ping', 4])
   });
 
-  it("will return a string 'ping' for (the multiple of 3) 6", function(){
-    expect(pingpong(7)).to.eql([1, 2, 'ping', 4, 5, 'ping', 7])
+  it("will return a string 'pong' for the number 5", function(){
+    expect(pingpong(5)).to.eql([1, 2, 'ping', 4, 'pong'])
+  });
+
+  it("will return a string 'ping' for (the multiples of 3) 6, 9", function(){
+    expect(pingpong(9)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping'])
+  });
+
+  it("will return a string 'pong' for the number 10", function(){
+    expect(pingpong(10)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong'])
+  });
+
+  it("will, up to the number 15, return a string 'ping' for all multples of 3, pong for all multiples of 5, and return 'pingpong' for the number 15", function(){
+    expect(pingpong(15)).to.eql([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'pingpong'])
+  });
+
+  it("will return 'pingpong' for the all multiples of 15", function(){
+    expect(pingpong(45)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong", 16, 17, "ping", 19, "pong", "ping", 22, 23, "ping", "pong", 26, "ping", 28, 29, "pingpong", 31, 32, "ping", 34, "pong", "ping", 37, 38, "ping", "pong", 41, "ping", 43, 44, "pingpong"])
   });
 });
