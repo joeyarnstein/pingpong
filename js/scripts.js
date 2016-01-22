70e2d9023d0f8db179450b6444f3bf402d0b1c89
Joey <joeyarnstein@gmail.com>
2016-01-22 14:09:14 -0800

    add limits to input, change input to only accept keystrokes of numerical value

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 var pingpong = function(countTo){
-  var returnArray = [];
-  for(var index = 1; index <= countTo; index += 1)
-    if (index % 15 === 0){
-      returnArray.push("pingpong");
-    } else if (index % 3 === 0) {
-      returnArray.push("ping");
-    } else if (index % 5 === 0){
-      returnArray.push("pong");
-    } else {
-      returnArray.push(index);
-    }
-  return returnArray;
+  if ((countTo > 0) && (countTo < 10000)) {
+    var returnArray = [];
+    for(var index = 1; index <= countTo; index += 1)
+      if (index % 15 === 0){
+        returnArray.push("pingpong");
+      } else if (index % 3 === 0) {
+        returnArray.push("ping");
+      } else if (index % 5 === 0){
+        returnArray.push("pong");
+      } else {
+        returnArray.push(index);
+      }
+    return returnArray;
+  } else {
+    alert ("Don't be silly now");
+  }
 }
 
 $(document).ready(function() {
